174a30347e241d42db2f26223e8462244103a4eb
ytk6565 <ytk6565@gmail.com>
2021-09-06 20:27:23 +0900

    fix: eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [],
   // add your custom rules here
   rules: {},
+  ignorePatterns: ['src/static/js/opencv.js'],
   overrides: [
     {
       files: ['**/*.{spec,test}.{j,t}s?(x)'],
